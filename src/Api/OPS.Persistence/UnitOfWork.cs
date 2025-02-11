b33adb37549cd50411e5905d8be9fd9885084e5d
Rawfin <zaidaminraofin@gmail.com>
2025-02-11 07:41:05 +0600

    Automate soft delete functionality

--- src/Api/OPS.Persistence/UnitOfWork.cs
+++ src/Api/OPS.Persistence/UnitOfWork.cs
@@ -1,22 +1,35 @@
-﻿using OPS.Domain;
+﻿using Microsoft.EntityFrameworkCore;
+using OPS.Domain;
 using OPS.Domain.Contracts;
+using OPS.Domain.Entities.Common;
 
 namespace OPS.Persistence;
 
-internal class UnitOfWork(AppDbContext dbContext, IExamRepository examRepository, IAccountRepository accountRepository) : IUnitOfWork
+internal class UnitOfWork(AppDbContext dbContext, IExamRepository examRepository, IAccountRepository accountRepository)
+    : IUnitOfWork
 {
-    private readonly AppDbContext _context = dbContext;
+    private readonly AppDbContext _dbContext = dbContext;
 
     public IExamRepository Exam { get; } = examRepository;
     public IAccountRepository Account { get; } = accountRepository;
 
     public async Task<int> CommitAsync(CancellationToken cancellationToken = default)
     {
-        return await _context.SaveChangesAsync(cancellationToken);
+        var softDeletableEntity = _dbContext.ChangeTracker
+            .Entries<ISoftDeletable>()
+            .Where(entry => entry.State == EntityState.Deleted);
+
+        foreach (var entityEntry in softDeletableEntity)
+        {
+            entityEntry.Property(nameof(ISoftDeletable.IsDeleted)).CurrentValue = true;
+            entityEntry.State = EntityState.Modified;
+        }
+
+        return await _dbContext.SaveChangesAsync(cancellationToken);
     }
 
     public void Dispose()
     {
-        _context.Dispose();
+        _dbContext.Dispose();
     }
 }
