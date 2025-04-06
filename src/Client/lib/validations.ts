806987c60261567050d77e57ccf08be6cfa3c32e
akhi005 <kohinooraktherakhi5539@gmail.com>
2025-04-06 23:07:45 +0600

    fix regex

--- src/Client/lib/validations.ts
+++ src/Client/lib/validations.ts
@@ -4,12 +4,15 @@ export const SignInSchema = z.object({
   email: z
     .string()
     .min(1, { message: 'Email is required' })
-    .email({ message: 'Please provide a valid email address.' }),
+    .regex(/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/,{ message: 'Please provide a valid email address.' }),
 
-  password: z
+    password: z
     .string()
-    .min(8, { message: 'Password must be at least 8 characters long. ' })
-    .max(100, { message: 'Password cannot exceed 100 characters.' }),
+    .min(8, { message: 'Password must be at least 8 characters long.' })
+    .max(100, { message: 'Password cannot exceed 100 characters.' })
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$/, {
+      message: 'Password must contain at least one uppercase letter,one lowercase letter,one number, one special character.',
+    }),
 })
 
 export const signUpSchema = z
@@ -24,21 +27,14 @@ export const signUpSchema = z
     email: z
       .string()
       .min(1, { message: 'Email is required.' })
-      .email({ message: 'Please provide a valid email address.' }),
+      .regex(/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/,{ message: 'Please provide a valid email address.' }),
 
     password: z
       .string()
       .min(8, { message: 'Password must be at least 8 characters long.' })
       .max(100, { message: 'Password cannot exceed 100 characters.' })
-      .regex(/[A-Z]/, {
-        message: 'Password must contain at least one uppercase letter.',
-      })
-      .regex(/[a-z]/, {
-        message: 'Password must contain at least one lowercase letter.',
-      })
-      .regex(/[0-9]/, { message: 'Password must contain at least one number.' })
-      .regex(/[^a-zA-Z0-9]/, {
-        message: 'Password must contain at least one special character.',
+      .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$/, {
+        message: 'Password must contain at least one uppercase letter,one lowercase letter,one number, one special character.',
       }),
     confirmPassword: z.string(),
 
