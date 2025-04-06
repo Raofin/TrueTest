0595cfffdc88b63d0f5d63d9a68b02a62863c666
Rawfin <zaidaminraofin@gmail.com>
2025-04-06 14:48:50 +0600

    Update database migration configurations

--- src/Api/OPS.Infrastructure/AppConfigurations.cs
+++ src/Api/OPS.Infrastructure/AppConfigurations.cs
@@ -78,7 +78,30 @@ public static class AppConfigurations
         var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
         var databaseCreator = dbContext.GetService<IRelationalDatabaseCreator>();
 
-        if (!databaseCreator.Exists()) dbContext.Database.Migrate();
+        if (!databaseCreator.Exists())
+        {
+            Log.Information("Database does not exist. Creating it and applying migrations...");
+            dbContext.Database.Migrate();
+        }
+        else
+        {
+            var connection = dbContext.Database.GetDbConnection();
+            connection.Open();
+
+            using var command = connection.CreateCommand();
+            command.CommandText = "SELECT TOP 1 * FROM Enum.Roles";
+
+            try
+            {
+                command.ExecuteReader();
+                Log.Information("Database already exists and is up to date.");
+            }
+            catch
+            {
+                Log.Information("Database exists but is not up to date. Applying migrations...");
+                dbContext.Database.Migrate();
+            }
+        }
     }
 
     #endregion
