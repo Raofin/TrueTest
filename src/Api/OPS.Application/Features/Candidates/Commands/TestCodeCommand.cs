87cd6169fffce464466ad878801061b4430213c5
Rawfin <zaidaminraofin@gmail.com>
2025-04-19 15:38:42 +0600

    Add OneCompiler API in test case execution

--- src/Api/OPS.Application/Features/Candidates/Commands/TestCodeCommand.cs
+++ src/Api/OPS.Application/Features/Candidates/Commands/TestCodeCommand.cs
@@ -1,7 +1,9 @@
 ﻿using ErrorOr;
 using FluentValidation;
 using MediatR;
+using OPS.Application.Common.Extensions;
 using OPS.Domain;
+using OPS.Domain.Contracts.Core.OneCompiler;
 using OPS.Domain.Enums;
 
 namespace OPS.Application.Features.Candidates.Commands;
@@ -9,33 +11,45 @@ namespace OPS.Application.Features.Candidates.Commands;
 public record TestCodeResponse(
     Guid TestCaseId,
     bool IsAccepted,
-    string ReceivedOutput,
-    TimeSpan ExecutionTime
+    string? ReceivedOutput,
+    int? ExecutionTime
 );
 
-public record TestCodeCommand(Guid QuestionId, string Code, ProgLanguageType ProgLanguageType)
+public record TestCodeCommand(Guid QuestionId, string Code, LanguageId LanguageId)
     : IRequest<ErrorOr<List<TestCodeResponse>>>;
 
-public class TestCodeCommandHandler(IUnitOfWork unitOfWork)
+public class TestCodeCommandHandler(IUnitOfWork unitOfWork, IOneCompilerApiService oneCompilerApiService)
     : IRequestHandler<TestCodeCommand, ErrorOr<List<TestCodeResponse>>>
 {
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IOneCompilerApiService _oneCompilerApi = oneCompilerApiService;
 
-    public async Task<ErrorOr<List<TestCodeResponse>>> Handle(TestCodeCommand request,
-        CancellationToken cancellationToken)
+    public async Task<ErrorOr<List<TestCodeResponse>>> Handle(
+        TestCodeCommand request, CancellationToken cancellationToken)
     {
         var testCases = await _unitOfWork.TestCase.GetByQuestionIdAsync(request.QuestionId, cancellationToken);
 
-        // TODO: Add compiler service to compile the code
+        var responses = new List<TestCodeResponse>();
 
-        return testCases.Select(
-            testCase => new TestCodeResponse(
+        foreach (var testCase in testCases)
+        {
+            var result = await _oneCompilerApi.CodeRunAsync(
+                request.LanguageId,
+                request.Code,
+                testCase.Input
+            );
+
+            var response = new TestCodeResponse(
                 testCase.Id,
-                false,
-                "Compiler error",
-                new TimeSpan(10)
-            )
-        ).ToList();
+                testCase.ExpectedOutput.TrimEnd() == result.Stdout?.TrimEnd(),
+                result.Stdout,
+                result.ExecutionTime
+            );
+
+            responses.Add(response);
+        }
+
+        return responses;
     }
 }
 
@@ -44,13 +58,12 @@ public class TestCodeCommandValidator : AbstractValidator<TestCodeCommand>
     public TestCodeCommandValidator()
     {
         RuleFor(x => x.QuestionId)
-            .NotEmpty()
-            .NotEqual(Guid.Empty);
+            .IsValidGuid();
 
         RuleFor(x => x.Code)
             .NotEmpty();
 
-        RuleFor(x => x.ProgLanguageType)
+        RuleFor(x => x.LanguageId)
             .IsInEnum();
     }
 }
